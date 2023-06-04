f9a3dfefc31762875a4c29585fe64901e951ab6f
Katlin Sampson <katlin.sampson@wwt.com>
2023-06-04 13:45:46 -0500

    adding port_details to rogue_mac_ports

--- security.py
+++ security.py
@@ -75,35 +75,51 @@ def rogue_bpdu_switches(switch_list, user, pwd=None) -> None:
 def rogue_mac_ports(switch_list, user, pwd=None) -> None:
     '''
     '''
+    if switch_list.endswith('yml'):
+        switch_list = file_loader(switch_list)
+
     switch_list = format_switch_list(
         switch_list, user, pwd=pwd)
 
     switch_list = switch_list_send_command(
-        switch_list, 'show mac address-table',
-        fsm=True, fsm_template='nil_lib/templates/mac_address_table')
+        switch_list, ['show mac address-table', 'sh run'])
+
+    fsm_mac = file_loader(
+        'nil_lib/templates/mac_address_table.fsm')
+    fsm_interfaces = file_loader(
+        'nil_lib/templates/interfaces.fsm')
 
     audit_list = []
     for switch in switch_list:
+        if not switch['name']:
+            break
+
+        fsm_mac._result, fsm_interfaces._result = [], []
+        switch['output'][0] = fsm_mac.ParseTextToDicts(
+            switch['output'][0])
+        switch['output'][1] = fsm_interfaces.ParseTextToDicts(
+            switch['output'][1])
+
         mac_list, processed_list = [], []
-        for check_entry in switch['output'][:]:
-            if check_entry['ports'] in processed_list:
+        for check_entry in switch['output'][0][:]:
+            if check_entry['PORTS'] in processed_list:
                 continue
-            if ('CPU' in check_entry['ports']
-                or 'Po' in check_entry['ports']):
-                switch['output'].remove(check_entry)
+            if ('CPU' in check_entry['PORTS']
+                or 'Po' in check_entry['PORTS']):
+                switch['output'][0].remove(check_entry)
                 continue
             found = []
-            for entry in switch['output']:
+            for entry in switch['output'][0]:
                 if (check_entry == entry
-                    or entry['ports'] in processed_list):
+                    or entry['PORTS'] in processed_list):
                     continue
-                if check_entry['ports'] == entry['ports']:
+                if check_entry['PORTS'] == entry['PORTS']:
                     found.append(entry)
 
             list_check = [
-                {'key': 'vlan', 'value': check_entry['vlan']},
-                {'key': 'mac', 'value': check_entry['mac']},
-                {'key': 'type', 'value': check_entry['type']}]
+                {'key': 'VLAN', 'value': check_entry['VLAN']},
+                {'key': 'MAC', 'value': check_entry['MAC']},
+                {'key': 'TYPE', 'value': check_entry['TYPE']}]
             for check in list_check:
                 if not isinstance(check['value'], list):
                     check_entry[check['key']] = [check['value']]
@@ -111,22 +127,41 @@ def rogue_mac_ports(switch_list, user, pwd=None) -> None:
             if found:
                 for entry in found:
                     list_check = [
-                        {'key': 'vlan', 'value': entry['vlan']},
-                        {'key': 'mac', 'value': entry['mac']},
-                        {'key': 'type', 'value': entry['type']}]
+                        {'key': 'VLAN', 'value': entry['VLAN']},
+                        {'key': 'MAC', 'value': entry['MAC']},
+                        {'key': 'TYPE', 'value': entry['TYPE']}]
                     for check in list_check:
                         if entry[check['key']] not in check_entry[check['key']]:
                             check_entry[check['key']].append(check['value'])
 
-                    switch['output'].remove(entry)
+                    switch['output'][0].remove(entry)
 
-            processed_list.append(check_entry['ports'])
-            if len(check_entry['mac']) > 2:
-                mac_list.append(check_entry['ports'])
+            processed_list.append(check_entry['PORTS'])
+            if len(check_entry['MAC']) > 2:
+                mac_list.append(check_entry['PORTS'])
+
+        if mac_list:
+            mac_list = sorted(mac_list)
+            for port in mac_list[:]:
+                found = ''
+                if 'Gi' in port:
+                    for interface in switch['output'][1]:
+                        if port[2:] in interface['INTERFACE_NAME']:
+                            found = interface['INTERFACE_DETAILS']
+                            for detail in interface['INTERFACE_DETAILS']:
+                                if 'switchport mode trunk' in detail:
+                                    found = ''
+                                    mac_list.remove(port)
+                                    break
+                            break
+
+                if found:
+                    mac_list.remove(port)
+                    mac_list.append({'port_name': port, 'port_details': found})
 
         if mac_list:
             audit_list.append({
-                'name': switch['name'], 'ports': sorted(mac_list)})
+                'name': switch['name'], 'ports': mac_list})
 
     file_create(
         'rogue_mac_ports',
